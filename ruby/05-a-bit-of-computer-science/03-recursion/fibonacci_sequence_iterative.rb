a3165b775186b7f6a11864760e0df6d1a378f7a9
Husayn Hakeem <husaynhakeem@gmail.com>
2020-06-07 18:51:34 -0700

    Add solution to iterative fibonacci sequence

---
+++ ruby/05-a-bit-of-computer-science/03-recursion/fibonacci_sequence_iterative.rb
@@ -0,0 +1,15 @@
+def fibonacci_sequence(n)
+  sequence = []
+  0.upto(n) do |i|
+    sequence << fibonacci(i)
+  end
+  sequence
+end
+
+def fibonacci(n)
+  fib = [0, 1]
+  2.upto(n) do |i|
+    fib << fib[i - 1] + fib[i - 2]
+  end
+  fib[n]
+end
